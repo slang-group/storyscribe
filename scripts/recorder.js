9af1978b4df7db6185435ac21aab106a8e8e880b
Nick Doiron <ndoiron@mapmeld.com>
2013-10-07 10:48:48 -0400

    support non-Webkit AudioContext

--- scripts/recorder.js
+++ scripts/recorder.js
@@ -6,7 +6,12 @@
     var config = cfg || {};
     var bufferLen = config.bufferLen || 4096;
     this.context = source.context;
-    this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    if(typeof this.context.createJavaScriptNode == "function"){
+      this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    }
+    else{
+      this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+    }
     var worker = new Worker(config.workerPath || WORKER_PATH);
     worker.postMessage({
       command: 'init',
